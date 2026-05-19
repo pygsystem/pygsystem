4ed2a6dc433077b81fc040f2212fd0cc0560690b
pygsystem <pgarate.sinohydro@gmail.com>
2026-05-19 10:47:43 -0500

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,7 @@ export default function MultiserviciosPyGSystem() {
               </div>
 
               <div className="bg-white p-8 rounded-3xl shadow-lg border border-slate-200">
-                <h3 className="text-3xl font-bold text-blue-700 mb-4">paul16garate@gmail.com</h3>
+                <h3 className="text-3xl font-bold text-blue-700 mb-4">pygsystemweb@gmail.com</h3>
                 <p className="text-2xl text-slate-600">Envíanos un correo</p>
               </div>
             </div>
